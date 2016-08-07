e0e539c58403afd671f51e521fef262839816c9b
Eakkasit tunsakool <julongdragon@gmail.com>
2016-08-07 07:28:38 +0700

    init02

--- Example/eakkasit/ViewController.swift
+++ Example/eakkasit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         var number : Int = 456
+        
         print(number.Hex)
         
     }
--- eakkasit.podspec
+++ eakkasit.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'eakkasit'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of eakkasit.'
+  s.summary          = 'Hello My Pod'
 
 
 
